e8bc4203d4dedf9dbae27851abfc79a95f0a244e
Wess Lancaster <wess.lancaster@gmail.com>
2019-12-08 12:57:09 -0700

    Description

---
+++ CacheMoney/CacheMoney/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CacheMoney
+{
+    class Program
+    {
+        /// <summary>
+        /// Receives a series of decimal addresses, and simulates cache entries and access as well
+        /// as hit/miss info. Uses 16-bit addresses and 32-bit instructions.
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
