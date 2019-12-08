9da7c602ab9fbfaa39f528db92c9c3f3c5f0c990
Wess Lancaster <wess.lancaster@gmail.com>
2019-12-08 14:04:28 -0700

    Display demo

--- CacheMoney/CacheMoney/Program.cs
+++ CacheMoney/CacheMoney/Program.cs
@@ -15,7 +15,19 @@ namespace CacheMoney
         /// <param name="args"></param>
         static void Main(string[] args)
         {
-
+            //  Display Demo
+            Console.WriteLine("Direct-Mapped Cache: 8 Byte Blocks, 8 Rows");
+            Console.WriteLine("Cache size: X Rows * 1 Valid bit * X Tag bits * X X-byte blocks = 683 bits");
+            Console.WriteLine("16-bit address -> | 22-bit tag | 8 Rows | 2 Offset |");
+            Console.WriteLine("Cache miss time: 1 cycle + 10 cycle penalty + 8 Byte Blocks per cycle = 19 cycles");
+            Console.WriteLine("Address: |0|4|8|...");
+            Console.WriteLine("Hit/Miss: |M|H|M|...");
+            Console.WriteLine("X hits, X misses. X% hit rate.");
+            Console.WriteLine("(Avg CPI = X hits + X misses * X-cycle miss penalty) / X address lookups = X Avg CPI");
+            Console.WriteLine("Row|Valid|Tag");
+            for (int i = 0; i < 3; i++)
+                Console.WriteLine(i + "|1|01100");
+            Console.Read();
         }
     }
 }
